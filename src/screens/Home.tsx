84068b72694c83527deeab390de6d06c08735a6a
Muhammad Hilal <muhammadhilal494513@gmail.com>
2024-12-25 10:14:16 +0500

    Home screen setting for navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+// navigation
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+import {RootStackParamList} from '../App'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+export default function Home({navigation}: HomeProps) {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <View style={styles.container}>
+    <Text style={styles.smallText}>Home Screen</Text>
+    <Button
+    title='Go to details'
+    // onPress={() => navigation.navigate("Details", {
+    //   productId: "86"
+    // })}
+    
+    // onPress={() => navigation.navigate("Details")}
+
+    onPress={() => navigation.push('Details', {
+      productId: "86"
+    })}
+
+    />
+  </View>
   )
 }
 
